449a0e26b5cde2b1b9d02ef789fb92479e2796ec
spike <guang.spike.yang@gmail.com>
2026-03-15 20:44:05 +1100

    chore(App.vue): remove Vite and Vue logos and keep it clean.

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,10 @@
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="p-6">
+    <h1 class="text-xl font-semibold">
+      Mewgenics Cat Smuggling
+    </h1>
   </div>
 </template>
 
